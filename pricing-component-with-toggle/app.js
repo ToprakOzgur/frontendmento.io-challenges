0a0456423bcde225bcc1148212162cc2ae076a51
ToprakOzgur <zgrtprk@gmail.com>
2020-10-05 15:32:47 +0300

    js ok

--- pricing-component-with-toggle/app.js
+++ pricing-component-with-toggle/app.js
@@ -1,1 +1,21 @@
+const basicPrice = document.getElementById('basic');
+const proPrice = document.getElementById('pro');
+const masterPrice = document.getElementById('master');
 
+const toggle = document.getElementById('toggle');
+
+let annualSelected = true;
+
+toggle.addEventListener('click', () => {
+  annualSelected = !annualSelected;
+
+  if (annualSelected) {
+    basicPrice.innerHTML = '$199.99';
+    proPrice.innerHTML = '$249.99';
+    masterPrice.innerHTML = '$399.99';
+  } else {
+    basicPrice.innerHTML = '$19.99';
+    proPrice.innerHTML = '$24.99';
+    masterPrice.innerHTML = '$39.99';
+  }
+});
